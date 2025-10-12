a84d6c271bf6c2ba4bfeae630a727ffbc9fc1677
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-10-12 11:23:56 +0200

    Fix NotificationsModule dependency injection by importing UsersModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { DeviceToken } from './device-token.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DeviceToken])],
+  imports: [
+    TypeOrmModule.forFeature([DeviceToken]),
+    UsersModule,
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
