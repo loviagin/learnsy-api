bfac00cdd53d547fe8f5df381dc92fb868ab6316
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-10-12 10:39:43 +0200

    Fix: Import UsersModule in ChatsModule to resolve UsersService dependency

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,10 +4,12 @@ import { ChatsController } from './chats.controller';
 import { ChatsService } from './chats.service';
 import { Chat, ChatParticipant, ChatMessage } from './chat.entity';
 import { AppUser } from '../users/app-user.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Chat, ChatParticipant, ChatMessage, AppUser]),
+    UsersModule,
   ],
   controllers: [ChatsController],
   providers: [ChatsService],
